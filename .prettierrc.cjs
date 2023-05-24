bcc0ed7ee44f367f382b16b3e48b551474d29865
irfandi <irfandi.nutech@gmail.com>
2023-05-24 16:16:53 +0700

    [Add] - Setup project

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+/* eslint-env node */
+
+module.exports = {
+	endOfLine: 'lf',
+	jsxSingleQuote: false,
+	printWidth: 120,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'es5',
+	useTabs: true,
+};
