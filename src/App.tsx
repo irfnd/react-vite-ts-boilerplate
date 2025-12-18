3215a6e7c53fa82dbe228b21bfbaca41f07751e5
Irfandi Iqbal Abimanyu <irfndiabimanyu@gmail.com>
2025-12-18 08:26:00 +0700

    refactor: Cleaning existing boilerplate

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-	return (
-		<div>
-			<h1>Welcome to React + TypeScript Boilerplate</h1>
-			<p>A boilerplate/starter project for quickly building web using React with Vite and Typescript</p>
-		</div>
-	);
-}
